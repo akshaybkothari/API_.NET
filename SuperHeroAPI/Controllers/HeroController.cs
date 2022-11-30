743bd8919c9fc65b2b602a3b7eef7b633e403bb8
akshay kothari <akshay.kothari@jda.com>
2022-11-30 13:04:00 +0530

    First commit

---
+++ SuperHeroAPI/Controllers/HeroController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : ControllerBase
+    {
+        public static List<Hero> MyHero = new List<Hero>
+            {
+                new Hero
+                {
+                    Id = 1,
+                    name = "superman",
+                    address = "kar"
+
+                }
+
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Hero>>> Get()
+        {
+
+            return Ok(MyHero);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hero>> Get(int id)
+        {
+            var hero = MyHero.Find(h => h.Id == id);
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Hero>>> Post(Hero hero)
+        {
+            MyHero.Add(hero);
+            return Ok(MyHero);
+        }
+    }
+}
